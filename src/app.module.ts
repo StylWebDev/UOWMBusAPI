1a2c222e2fc91cba2d06c7754f8852ea9f96ff05
AliBaBas02 <kostasstilianou@gmail.com>
2024-09-18 10:56:40 +0300

    Updated main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,20 @@ import { MorganInterceptor } from 'nest-morgan';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { MorganModule } from 'nest-morgan';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [MorganModule, ServeStaticModule.forRoot({ rootPath: './client' })],
+  imports: [
+    MorganModule,
+    ServeStaticModule.forRoot({ rootPath: './client' }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [BusController],
   providers: [
     BusService,
     {
       provide: APP_INTERCEPTOR,
-      useClass: MorganInterceptor('combined'),
+      useClass: MorganInterceptor("combined"),
     },
   ],
 })

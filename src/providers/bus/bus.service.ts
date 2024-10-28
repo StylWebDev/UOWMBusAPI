0c3758c7740c7fa1b7dc7499366d38fc156302d8
AliBaBas02 <kostasstilianou@gmail.com>
2024-10-29 00:34:15 +0200

    Added city info file

--- src/providers/bus/bus.service.ts
+++ src/providers/bus/bus.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as tei from '../../util/schedules/tei.json';
 import * as mesopotamia from '../../util/schedules/mesopotamia.json';
 import * as maniakoi from '../../util/schedules/maniakoi.json';
+import * as info from '../../util/areas/info.json';
 
 @Injectable()
 export class BusService {
@@ -13,6 +14,8 @@ export class BusService {
         return maniakoi;
       case '3':
         return mesopotamia;
+      case '52100':
+        return info;
       default:
         throw new HttpException('Invalid schedule', HttpStatus.BAD_REQUEST);
     }
